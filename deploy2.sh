013d4a7dad3008fb98d8866d0f2292358a841674
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-06-04 04:56:27 +0700

    Create deploy2.sh

---
+++ deploy2.sh
@@ -0,0 +1,10 @@
+wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -qq -n ngrok-stable-linux-amd64.zip
+apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
+echo root:persija123* | chpasswd
+mkdir -p /var/run/sshd
+sudo -i
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+cd /etc/init.d/sshd start
+./ngrok authtoken 3nRWBTBrLBNwoNw183wGu_6qvZ8wTVZM1BMHpi18Z4H && ./ngrok tcp 22
