67068d8edf04004df001ba887cd6e83183518258
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-06-04 04:59:08 +0700

    Update and rename deploy2.sh to deploy3.sh

--- deploy3.sh
+++ deploy3.sh
@@ -1,9 +1,9 @@
 wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -qq -n ngrok-stable-linux-amd64.zip
+sudo -i
 apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
 echo root:persija123* | chpasswd
 mkdir -p /var/run/sshd
-sudo -i
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 cd /etc/init.d/sshd start
